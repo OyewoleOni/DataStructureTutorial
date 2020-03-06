81645a0f5e31b7cc4772114539f8ec7377dce96e
Oyewole Oni <ooni@eminenttechnology.com>
2020-03-06 13:20:50 +0100

    changes

--- LinkedList_1/LinkedList.cs
+++ LinkedList_1/LinkedList.cs
@@ -87,8 +87,67 @@ namespace LinkedList_1
 
         public object Add(object o)
         {
-
+            return this.Add(count, o);
         }
-        
+
+        public object Remove(int index)
+        {
+            if(index < 0)
+                throw  new ArgumentOutOfRangeException($"Index: {index}");
+
+            if (this.Empty)
+                return null;
+            if (index > this.count)
+                index = count - 1;
+
+            Node current = this.head;
+
+            object result = null;
+
+            if (index == 0)
+            {
+                result = current.Data;
+                this.head = current.Next;
+            }
+            else
+            {
+                for (int i = 0; i < index - 1; i++)
+                {
+                    current = current.Next;
+                }
+
+                result = current.Next.Data;
+                current.Next = current.Next.Next;
+            }
+
+            count--;
+            return result;
+        }
+
+        public void Clear()
+        {
+            this.head = null;
+            this.count = 0;
+        }
+
+        public int IndexOf(object o)
+        {
+            Node current = this.head;
+
+            for (int i = 0; i < this.count; i++)
+            {
+                if (current.Data.Equals(o))
+                    return i;
+                current = current.Next;
+            }
+
+            return -1;
+            
+        }
+
+        public bool Contains(object o)
+        {
+            return this.IndexOf(o) >= 0;
+        }
     }
 }
--- LinkedList_1/Program.cs
+++ LinkedList_1/Program.cs
@@ -12,9 +12,21 @@ namespace LinkedList_1
         {
             LinkedList list = new LinkedList();
 
-            Console.WriteLine($"Is it empty? - {list.Empty}" );
+            //Console.WriteLine($"Is it empty? - {list.Empty}" );
 
-            Console.WriteLine($"Count - {list.Count}");
+            //Console.WriteLine($"Count - {list.Count}");
+
+            //list.Add("Test1");
+            //list.Add("Test2");
+            //list.Add(1, "Test3");
+
+
+            //list.Remove(1);
+            list.Add("Hello");
+            list.Add("Test");
+            int index = list.IndexOf("Hello");
+
+            bool contains = list.Contains("Test");
 
             Console.ReadLine();
         }
